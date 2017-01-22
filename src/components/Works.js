b4e0c71af3b984c99bebfbf6007dda1478c56d34
Robert Zehnder <robzcodes@gmail.com>
2017-01-22 12:03:36 -0500

    react-create-app on Heroku

--- src/components/Works.js
+++ src/components/Works.js
@@ -6,9 +6,9 @@ class Works extends Component {
       <div className="worksContainer">
         <h1 className="works">WORKS</h1>
         <div className="projects">
-          <a href="https://robz-simon.herokuapp.com/index.html"> <div className="simon picture"></div> </a>
           <a href="https://pgatour-api-by-robz.herokuapp.com/"> <div className="pgaapi picture"></div> </a>
           <a href="https://robertzehnder.github.io/#/welcome"> <div className="drift picture"></div> </a>
+          <a href="https://robz-simon.herokuapp.com/index.html"> <div className="simon picture"></div> </a>
         </div>
       </div>
     )
